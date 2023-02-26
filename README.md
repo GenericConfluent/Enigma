90b9fadaf16b6c6b0063649dc0e490e1a99f1f4c
GenericConfluent <m13power@gmail.com>
2023-02-25 19:14:30 -0700

    2023 Redo

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Enigma
-This repository is basically a redo of a failed enigma machine that I attempted to create a year ago when I was new to C++. It was a project that was too ambitious for my skills at the time. So I left the project and decided to just work on improving my skills in the language. Then I forgot about it. On April 26, 2019, I was looking through my repositories(most of which are private) I saw it and thought I ought to attempt redoing it. This is that attempt, so if you feel like it download the code yourself and explore it.
+
+This repository is basically a redo of a failed enigma machine that I attempted
+to create a year ago when I was new to C++. It was a project that was too
+ambitious for my skills at the time. So I left the project and decided to just
+work on improving my skills in the language. Then I forgot about it. On April
+26, 2019, I was looking through my repositories(most of which are private) I
+saw it and thought I ought to attempt redoing it. This is that attempt, so if
+you feel like it download the code yourself and explore it.
+
+# 2023 Update
+Again it's been a couple years since I took on the enigma challenge and so now
+after having done a couple hundred programming puzzles I thought I'd give it a go.
+Except this time, I gave myself 30 minutes to add my fixes and edits.
+
+Which did not go according to plan. I accomplished less in 30 minutes than I
+had hoped to so I went like 2 hours over time. Also it basically became a
+layered shift cipher instead of an enigma machine. But it's neater now, so I'm
+happy.
+
--- main.cpp
+++ main.cpp
@@ -1,11 +1,69 @@
 #include <iostream>
 #include <string>
-#include "EnigmaMachine.hpp"
+#include <vector>
 
-int main(){
-  EnigmaMachine enigma;
+void assert_allowable_char(char ch) {
+  if (ch < 'A' || ch > 'Z') {
+    std::cout
+        << "Input text can only contain Uppercase alphabetic characters (A-Z)"
+        << std::endl;
+    std::exit(-1);
+  }
+}
 
-  std::string testcipher = enigma.cipher("test");
-  std::cout << "Test Cipher: " << testcipher << "\n";
-  std::cout << "Test Decipher: " << enigma.decipher(testcipher) << "\n";
+// NOTE: This is a really sucky enigma machine beause it doesn't accurately
+// reflect how a real machine would behave: the rotors don't turn (the same
+// letters in the input will be the same letters in output) and can each only
+// encode a shift cipher, in addition there is no plugboard.
+std::string enigma(std::string input, std::vector<unsigned> rotors,
+                   int should_decode) {
+  if (should_decode)
+    std::transform(rotors.begin(), rotors.end(), rotors.begin(),
+                   [](auto rotor) { return 26 - (rotor % 26); });
+
+  std::string coded;
+  for (auto i = 0; i < input.size(); ++i) {
+    char ch = input[i];
+    assert_allowable_char(ch);
+    ch -= 'A';
+
+    for (const unsigned &rotor : rotors)
+      ch = (ch + rotor) % 26;
+
+    // At least there is a reflector.
+    for (unsigned j = rotors.size(); --j;)
+      ch = (ch + rotors[j]) % 26;
+
+    coded.push_back(ch + 'A');
+  }
+  return coded;
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(0);
+
+  size_t rotor_count;
+  std::cout << "How many rotors: ";
+  std::cin >> rotor_count;
+
+  std::vector<unsigned> rotors(rotor_count);
+  std::cout << "Enter a space separated list of positive rotor vals on row "
+               "below (i.e. 4 2 5): "
+            << std::endl;
+  for (auto &rotor : rotors)
+    std::cin >> rotor;
+
+  std::string input;
+  std::cout << "Enter machine input: ";
+  std::cin >> input;
+
+  std::transform(input.begin(), input.end(), input.begin(), ::toupper);
+
+  int should_decode;
+  std::cout << "Encode (Enter 0) or Decode (Enter 1): ";
+  std::cin >> should_decode;
+
+  std::string output = enigma(input, rotors, should_decode);
+  std::cout << output << std::endl;
 }
